32ee2c2a93c3a7b44e8842a1a12c416c137b0952
Julia Martins <juliamartins.td2.senai@gmail.com>
2025-04-22 10:48:39 -0300

    feat: add cloud back link

--- src/app/components/container6/index.jsx
+++ src/app/components/container6/index.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import styles from "./container6.module.css";
 
 const Container6 = () => {
-    const url = "http://localhost:4000/avaliacao";
+    const url = "https://front-back-bne4.onrender.com/avaliacao";
 
     const [avaliations, setAvaliations] = useState([]);
     const [loading, setLoading] = useState(true);
